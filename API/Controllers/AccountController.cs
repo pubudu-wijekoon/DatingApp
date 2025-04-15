d6083813f571020d2b86a32d857a39a59006a1dd
Pubudu Wijekoon <37551112+pubudu-wijekoon@users.noreply.github.com>
2025-04-16 06:57:37 +0900

    add DTOs and Refactor Program.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,17 +4,18 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
   [HttpPost("register")] //account/register
 
-  public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+  public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
   {
     if(await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
 
@@ -30,12 +31,16 @@ public class AccountController(DataContext context) : BaseApiController
     context.Users.Add(user);
     await context.SaveChangesAsync();
 
-    return user;
+    return new UserDto
+    {
+      Username = user.UserName,
+      Token = tokenService.CreateToken(user)
+    };
 
   }
 
   [HttpPost("login")]
-  public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+  public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
   {
     var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
     
@@ -50,7 +55,11 @@ public class AccountController(DataContext context) : BaseApiController
       if(computeHash[i] != user.PasswordHash[i]) return Unauthorized("invalid Password");
     }
 
-    return user;
+    return new UserDto
+    {
+      Username = user.UserName,
+      Token = tokenService.CreateToken(user)
+    };
   }
 
   private async Task<bool> UserExists(string username)
